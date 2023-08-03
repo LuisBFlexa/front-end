067adce028f1fe0ba0cb18bd5ede95fa3e130361
MatheusNevs <matheusnf62@gmail.com>
2023-08-03 18:27:29 -0300

    customizations in tailwind.config.js

--- projeto_final/tailwind.config.js
+++ projeto_final/tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     ],
-  theme: {
+  theme: {borderWidth: {
+    DEFAULT: '3px',
+    '0': '0',
+    '0.1':'0.1px',
+    '2': '2px',
+   '3': '3px',
+    '4': '4px',
+   '6': '6px',
+   '8': '8px',
+  },
     extend: {},
   },
   plugins: [],
