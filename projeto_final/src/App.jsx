b240e613e5ef4bf4fa590a725ad39ff2c8509f76
MatheusNevs <matheusnf62@gmail.com>
2023-08-06 22:31:01 -0300

    routes

--- projeto_final/src/App.jsx
+++ projeto_final/src/App.jsx
@@ -1,13 +1,51 @@
 import './index.css'
-import Navbar from './components/Navbar/index.jsx'
-import Homepage from './components/Homepage'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { Navbar } from './components/Navbar/navbar'
+import { Root } from './root/root'
+import Homepage from './pages/Homepage/homepage'
+import Profile from './pages/Profile/profile'
+import Post from './pages/Post/post'
+import LogIn from './user/login/login'
+import SignIn from './user/signin/signin'
+
+const router = createBrowserRouter([
+  {
+    path: "/", 
+    element: <Root/>,
+    children:[{
+      path: "",
+      element: <Homepage />
+    },
+    {
+      path:"profile",
+      element: <Profile />
+    },
+    {
+      path: "posts",
+      element: <Post />
+    },
+    {
+      path:"post/:id",
+      element:<Post />
+    },
+    {
+      path:"login",
+      element: <LogIn />
+    },
+    {
+      path:"signin",
+      element: <SignIn />
+    }
+  ]
+  }
+])
 
 function App() {
 
   return (
     <>
-      <Navbar />
-      <Homepage />
+      <RouterProvider router = { router } />
+
     </>
   )
 }
