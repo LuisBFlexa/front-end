a5b6ffef89e77976a5b16ab9056c9dbaa80a8519
Laguni <jpfsantos2004@gmail.com>
2023-08-10 13:49:04 -0300

    footer adjust

--- projeto_final/src/components/footer/footer.jsx
+++ projeto_final/src/components/footer/footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <div className="text-white w-full h-16 bg-blue-900 flex justify-end fixed bottom-0">
+        <div className="text-white w-full h-16 bg-blue-900 flex justify-end  static bottom-0">
             <div className="flex items-center space-x-10 mr-10 font-inter font-semibold ">
                 <ul>Desenvolvido pela Struct </ul>
                 <ul>2023</ul>
