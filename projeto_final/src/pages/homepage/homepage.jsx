f2c724edbca6e3b2d7c4a6a891a43212c37ce1fe
Laguni <jpfsantos2004@gmail.com>
2023-08-10 00:11:43 -0300

    att

--- projeto_final/src/pages/homepage/homepage.jsx
+++ projeto_final/src/pages/homepage/homepage.jsx
@@ -3,10 +3,10 @@ export default function Homepage() {
         
         <main className=" bg-[#F0EFEF]">
             
-            <h1 className="mt-10 ml-64 w-72 text-transparent bg-clip-text bg-gradient-to-r to-[#001971] from-[#c4c4c4]  tracking-wider font-bold font-serif text-4xl">CATEGORIAS</h1>
-            <div className=" flex justify-center space-x-60 mt-20 ">
+            <h1 className=" flex justify-center mt-14 text-transparent bg-clip-text bg-gradient-to-r to-[#001971] from-[#c4c4c4]  tracking-wider font-bold font-serif text-4xl">CATEGORIAS</h1>
+            <div className=" flex md:flex-wrap justify-center space-x-60 mt-20 ">
                 <div className="hover:saturate-200 shadow-lg shadow-[#001971] flex rounded-3xl bg-blue-900 w-96 ">
-                    <a href="#">
+                    <a href="#">    
                         <img className="  rounded-t-3xl h-48 w-96" src="https://cdn-media-1.freecodecamp.org/images/0*ngXgBNNdx6iiWP8q.png" alt="" />
                         <div className="p-4 ">
                         <h5 className=" hover:underline mb-2 text-2xl text-white text-center font-sans font-bold  leading-tight">Programação</h5>
--- projeto_final/src/pages/user/login/login.jsx
+++ projeto_final/src/pages/user/login/login.jsx
@@ -22,20 +22,20 @@ export default function LogIn () {
     return( 
         <main className="bg-[#F0EFEF]">
             <div className="flex justify-center items-center min-w-screen h-screen">
-                <div className=" bg-white shadow-xl w-1/3 h-2/3 mb-16 mt-20 flex flex-col rounded-2xl border-0.1 border-black">
+                <div className=" bg-white shadow-xl w-1/3 h-2/3 mb-16 mt-20 flex flex-col rounded-2xl border-0.1 border-black ">
                     <div className="mb-16">
                         <p className="text-black text-center text-2xl font-inter font-semibold mt-8"> Login </p>
                     </div>
 
                     <form className="justify-center mx-16" onSubmit={handleSubmit}>
-                        <div className=" flex flex-col h-fit w-full  space-y-10 justify-center">
+                        <div className=" flex flex-col h-fit w-full  space-y-10 justify-center ">
                             <input type="email" placeholder = 'EMAIL' className = 'border-0.1 border-black rounded-3xl h-10 text-center' value = {email} onChange={(ev) => {
                             setEmail(ev.target.value)}} />
                             <input type="password" placeholder = 'SENHA' className='border-0.1 border-black rounded-3xl h-10 text-center' value = {password} onChange={(ev) => {
                             setPassword(ev.target.value)}}/>
                         </div>
                         <div className="flex w-full h-fit justify-center items-center mt-10">
-                        <button type = "submit" className="bg-blue-900 w-1/2 h-12 rounded-3xl mt-10">
+                        <button type = "submit" className="bg-blue-900 w-1/2 h-12 rounded-3xl mt-10 ">
                             <p className="text-white text-2xl">Entrar</p>
                         </button>
                         </div>
