5faad1e318430330dcd59791121e9bb51f0ee283
Laguni <jpfsantos2004@gmail.com>
2023-08-11 00:36:54 -0300

    Post Page

--- projeto_final/src/App.jsx
+++ projeto_final/src/App.jsx
@@ -8,6 +8,8 @@ import LogIn from './pages/user/login/login'
 import SignIn from './pages/user/signin/signin'
 import Editdata from './pages/user/edit_data/edit_data'
 import { UserContextProvider } from './utils/UserContexts'
+import PostAdm from './pages/post/post_admin'
+import CreatePost from './pages/post/create_post'
 
 const router = createBrowserRouter([
   {
@@ -20,6 +22,14 @@ const router = createBrowserRouter([
     {
       path:"profile",
       element: <Profile />
+    },
+    {
+      path:"postadm",
+      element: <PostAdm />
+    },
+    {
+      path:"createpost",
+      element: <CreatePost />
     },
     {
       path:"edit_data",
--- projeto_final/src/pages/post/post_admin.jsx
+++ projeto_final/src/pages/post/post_admin.jsx
@@ -1,1 +1,40 @@
-// AINDA NAO FIZ
+function PostAdm() {
+    return (
+        <div className = "bg-white flex justify-around">
+            <div className="bg-[#F0EFEF] w-4/5 min-h-screen border-r-0.1 border-l-0.1 border-black justify-center">
+        
+                {/* CATEGORIA */}
+                <div className="mt-16 mx-8 w-max-full h-fit border-b-0.1 border-black">
+                    <p className="text-3xl">{post.category}</p>
+                </div>
+
+                <div className="mt-28 w-full max-h-screen justify-center items-center flex"> 
+                    <div className="bg-[#D9D9D9] w-1/2 h-fit max-h-4/5">
+                        <img src={post.picture} alt="foto do post" />
+                    </div>
+                </div>
+
+                <div className="my-20 w-full h-fit max-h-1/5 flex justify-center items-center">
+                    <div className="bg-[#D9D9D9] w-fit h-fit max-w-1/2 rounded-2xl border-0.1 border-black">
+                        <p className="p-4">
+                            {post.content}  
+                        </p>
+                    </div>
+                </div>
+                <div className="flex  justify-around items-center">
+                    <button type = "submit" className="bg-blue-900 w-40 h-12 rounded-3xl mt-10 my-10">
+                        <p className="text-white text-xl">Criar novo Post</p>
+                    </button>
+                    <button type = "submit" className="bg-blue-900 w-40 h-12 rounded-3xl mt-10 my-10">
+                        <p className="text-white text-xl">Excluir Post</p>
+                    </button>
+                    <button type = "submit" className="bg-blue-900 w-40 h-12 rounded-3xl mt-10d my-10">
+                        <p className="text-white text-xl">Editar Post</p>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PostAdm
